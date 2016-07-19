d3989c6fafeb73d528136894ae1122e4cc054436
Makoto Yamazaki <makoto1975@gmail.com>
2016-07-19 21:58:59 +0900

    17-3

--- app/src/androidTest/kotlin/org/zakky/qiitaclient/MainActivityTest.kt
+++ app/src/androidTest/kotlin/org/zakky/qiitaclient/MainActivityTest.kt
@@ -1,6 +1,8 @@
 package org.zakky.qiitaclient
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
@@ -12,6 +14,9 @@ import org.hamcrest.Matcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+import org.zakky.qiitaclient.client.ArticleClient
+import rx.Observable
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
@@ -30,4 +35,17 @@ class MainActivityTest {
     }
 
     fun isNotDisplayed(): Matcher<View> = not(isDisplayed())
+
+    @Test
+    fun `検索ボタンがタップされたら、入力されたクエリ文字列で記事検索APIを叩くこと`() {
+        val articleClient = mock(ArticleClient::class.java).apply {
+            `when`(search("user:ngsw_taro")).thenReturn(Observable.just(listOf()))
+        }
+        activityTestRule.activity.articleClient = articleClient
+
+        onView(withId(R.id.query_edit_text)).perform(typeText("user:ngsw_taro"))
+        onView(withId(R.id.search)).perform(click())
+
+        verify(articleClient).search("user:ngsw_taro")
+    }
 }
