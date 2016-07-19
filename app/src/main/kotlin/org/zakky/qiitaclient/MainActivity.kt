066ae6e0fe588716b78117402a9603da1e3d6070
Makoto Yamazaki <makoto1975@gmail.com>
2016-07-19 18:53:33 +0900

    16-2-2

--- app/src/main/kotlin/org/zakky/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/org/zakky/qiitaclient/MainActivity.kt
@@ -1,20 +1,27 @@
 package org.zakky.qiitaclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ListView
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity
+import com.trello.rxlifecycle.kotlin.bindToLifecycle
+import org.zakky.qiitaclient.client.ArticleClient
 import org.zakky.qiitaclient.model.Article
-import org.zakky.qiitaclient.model.User
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : RxAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val listAdapter = ArticleListAdapter(this)
-        listAdapter.articles = listOf(dummyArticle("123", "Kotlin入門", "たろう"),
-                dummyArticle("234", "Java入門", "じろう"))
 
         val listView = findViewById(R.id.list_view) as ListView
         listView.adapter = listAdapter
@@ -22,11 +29,32 @@ class MainActivity : AppCompatActivity() {
             val article = listAdapter.getItem(position) as Article
             ArticleActivity.intent(this, article).let { startActivity(it) }
         }
-    }
 
-    private fun dummyArticle(id: String, title: String, userName: String): Article =
-            Article(id = id,
-                    title = title,
-                    url = "http://example.com/article/${id}",
-                    user = User(id = "", name = userName, profileImageUrl = ""))
+        val gson = GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://qiita.com/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build()
+        val articleClient = retrofit.create(ArticleClient::class.java)
+
+        val queryEditText = findViewById(R.id.query_edit_text) as EditText
+        val searchButton = findViewById(R.id.search_button)
+
+        searchButton.setOnClickListener {
+            articleClient.search(queryEditText.text.toString())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .bindToLifecycle(this)
+                    .subscribe({
+                        queryEditText.text.clear()
+                        listAdapter.articles = it
+                        listAdapter.notifyDataSetChanged()
+                    }, {
+                        toast("エラー: $it")
+                    })
+        }
+    }
 }
--- app/src/main/kotlin/org/zakky/qiitaclient/view/ArticleView.kt
+++ app/src/main/kotlin/org/zakky/qiitaclient/view/ArticleView.kt
@@ -1,12 +1,12 @@
 package org.zakky.qiitaclient.view
 
 import android.content.Context
-import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import org.zakky.qiitaclient.R
 import org.zakky.qiitaclient.bindView
 import org.zakky.qiitaclient.model.Article
@@ -37,8 +37,6 @@ class ArticleView : FrameLayout {
     fun setArticle(article: Article) {
         titleTextView.text = article.title
         userNameTextView.text = article.user.name
-
-        // TODO プロフィール画像をセットする
-        profileImageView.setBackgroundColor(Color.RED)
+        Glide.with(context).load(article.user.profileImageUrl).into(profileImageView)
     }
 }
