8a3d8efa7bded18df73a2db0443de38128c6bb76
Makoto Yamazaki <makoto1975@gmail.com>
2016-07-19 21:32:03 +0900

    17-2

--- app/src/main/kotlin/org/zakky/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/org/zakky/qiitaclient/MainActivity.kt
@@ -6,24 +6,24 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import android.widget.ProgressBar
-import com.google.gson.FieldNamingPolicy
-import com.google.gson.GsonBuilder
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
 import com.trello.rxlifecycle.kotlin.bindToLifecycle
 import org.zakky.qiitaclient.client.ArticleClient
 import org.zakky.qiitaclient.model.Article
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
+import javax.inject.Inject
 
 class MainActivity : RxAppCompatActivity() {
 
+    @Inject
+    lateinit var articleClient: ArticleClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        (application as QiitaClientApp).component.inject(this)
 
+        setContentView(R.layout.activity_main)
 
         val listView = findViewById(R.id.list_view) as ListView
         val progressBar = findViewById(R.id.progress_bar) as ProgressBar
@@ -36,17 +36,6 @@ class MainActivity : RxAppCompatActivity() {
             val article = listAdapter.getItem(position) as Article
             ArticleActivity.intent(this, article).let { startActivity(it) }
         }
-
-        val gson = GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .create()
-        val retrofit = Retrofit.Builder()
-                .baseUrl("https://qiita.com/")
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .build()
-        val articleClient = retrofit.create(ArticleClient::class.java)
-
 
         searchButton.setOnClickListener {
             progressBar.visibility = View.VISIBLE
